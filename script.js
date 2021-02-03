3b3a3a82ec7ec9200e9d7961e1740d833da1a8a2
paullally <paullally98@gmail.com>
2021-02-03 16:45:12 +0000

    added modal and overlay for imgs

---
+++ script.js
@@ -0,0 +1,28 @@
+// Get the modal
+var modal = document.getElementsByClassName('modal');
+
+// Get the button that opens the modal
+var btn = document.getElementsByClassName("mybtn");
+
+
+// Get the <span> element that closes the modal
+var span = document.getElementsByClassName("close");
+
+// When the user clicks the button, open the modal 
+btn[0].onclick = function() {
+  modal[0].style.display = "block";
+}
+
+
+// When the user clicks on <span> (x), close the modal
+span[0].onclick = function() {
+  modal[0].style.display = "none";
+}
+
+
+// When the user clicks anywhere outside of the modal, close it
+window.onclick = function(event) {
+  if (event.target == modal) {
+    modal.style.display = "none";
+  }
+}
